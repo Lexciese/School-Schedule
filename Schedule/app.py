d9453771a8801ff45983cd4720aabc9305cb4fb6
ZAKI <falah.naufal.zk@gmail.com>
2021-01-30 17:24:51 +0700

    Refactored

--- Schedule/app.py
+++ Schedule/app.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 # from tkinter import ttk
-import json
-import os
+from json import loads
+from os import startfile
 from subprocess import Popen as OpenFile
 # from sys import *
 # from constructor import *
+
+
 
 
 def windowAlert():
@@ -30,7 +32,7 @@ def windowError():
     root = tk.Tk()
     root.title("Error")
     root.geometry('{}x{}'.format(Width, Height))
-    root.iconbitmap("Bin\\Images\\warning.ico")
+    # root.iconbitmap("Bin\\Images\\warning.ico")
     root.resizable(False, False)
 
     Error = tk.Label(root, text = "Error's Encounter\n\nPlease Check/ Fix your 'UserData.json'\nIf you Get Confused/ Don't Know What's Wrong\nYou can validate your 'UserData.json' at \nhttps://jsonlint.com/\nJust Copy/ Paste all of the 'UserData.json' content to that site \nand you will know what's wrong")
@@ -40,11 +42,11 @@ def windowError():
 
 
 def note():
-    os.startfile("Bin/Sticky Notes.lnk")
+    startfile("Bin/Sticky Notes.lnk")
 
 
 def StartRedirect():
-    os.startfile("C:/Developing/App/Python/Automated File Mover/dist/app.exe")
+    startfile("C:/Developing/App/Python/Automated File Mover/dist/app.exe")
 
 
 def ChangeSubjects():
@@ -67,268 +69,260 @@ def Help():
 
 
 class ManageData:
+    Size = ["700", "450"]
     jumlah = 0
-    Title = ""
-    Time = []
-    Senin = []
-    Selasa = []
-    Rabu = []
-    Kamis = []
-    Jumat = []
-    Sabtu = []
-    Day0 = ""
-    Day1 = ""
-    Day2 = ""
-    Day3 = ""
-    Day4 = ""
-    Day5 = ""
-    isDay0 = True
-    isDay1 = True
-    isDay2 = True
-    isDay3 = True
-    isDay4 = True
-    isDay5 = True
-    Icon = ""
+    Datas = {
+        "Icon": "",
+        "Title": "",
+        "Time": [],
+        "Days": {
+            "Day0": {
+                "Name": "",
+                "Exist": True,
+                "Sub": []
+            },
+            "Day1": {
+                "Name": "",
+                "Exist": True,
+                "Sub": []
+            },
+            "Day2": {
+                "Name": "",
+                "Exist": True,
+                "Sub": []
+            },
+            "Day3": {
+                "Name": "",
+                "Exist": True,
+                "Sub": []
+            },
+            "Day4": {
+                "Name": "",
+                "Exist": True,
+                "Sub": []
+            },
+            "Day5": {
+                "Name": "",
+                "Exist": True,
+                "Sub": []
+            }
+        }
+    }
     isError = False
 
 
     def __init__(self):
         try:
             data_ = open("Data/UserData.json")
-            data = json.loads(data_.read())
+            data = loads(data_.read())
 
-            for d in range(1):
-                Icon = data["Icon"]
-                self.Icon = Icon
+            for loop__ in range(1):
+                # self.Datas["Icon"] = data["Icon"]
+                self.Datas["Title"] = data["Title"]
+                self.Datas["Time"] = data["Time"]
 
-                Title = data["Title"]
-                self.Title = Title
-                
-                Time = data["Time"]
-                self.Time = Time
-
+                # Day0
                 if data["Day0"]["Exist"] == False:
-                    self.isDay0 = False
+                    self.Datas["Days"]["Day0"]["Exist"] = False
                 else:
-                    Senin = data["Day0"]["Sub"]
-                    Senin_ = data["Day0"]["Name"]
-                    self.Senin = Senin
-                    self.Day0 = Senin_
+                    self.Datas["Days"]["Day0"]["Name"], self.Datas["Days"]["Day0"]["Sub"] = data["Day0"]["Name"], data["Day0"]["Sub"]
 
+                # Day1
                 if data["Day1"]["Exist"] == False:
-                    self.isDay1 = False
+                    self.Datas["Days"]["Day1"]["Exist"] = False
                 else:
-                    Selasa = data["Day1"]["Sub"]
-                    Selasa_ = data["Day1"]["Name"]
-                    self.Selasa = Selasa
-                    self.Day1 = Selasa_
+                    self.Datas["Days"]["Day1"]["Sub"] = data["Day1"]["Sub"]
+                    self.Datas["Days"]["Day1"]["Name"] = data["Day1"]["Name"]
 
+                # Day2
                 if data["Day2"]["Exist"] == False:
-                    self.isDay2 = False
+                    self.Datas["Days"]["Day2"]["Exist"] = False
                 else:
-                    Rabu = data["Day2"]["Sub"]
-                    Rabu_ = data["Day2"]["Name"]
-                    self.Rabu = Rabu
-                    self.Day2 = Rabu_
+                    self.Datas["Days"]["Day2"]["Sub"] = data["Day2"]["Sub"]
+                    self.Datas["Days"]["Day2"]["Name"] = data["Day2"]["Name"]
 
+                # Day3
                 if data["Day3"]["Exist"] == False:
-                    self.isDay3 = False
+                    self.Datas["Days"]["Day3"]["Exist"] = False
                 else:
-                    Kamis = data["Day3"]["Sub"]
-                    Kamis_ = data["Day3"]["Name"]
-                    self.Kamis = Kamis
-                    self.Day3 = Kamis_
+                    self.Datas["Days"]["Day3"]["Sub"] = data["Day3"]["Sub"]
+                    self.Datas["Days"]["Day3"]["Name"] = data["Day3"]["Name"]
 
+                # Day4
                 if data["Day4"]["Exist"] == False:
-                    self.isDay4 = False
+                    self.Datas["Days"]["Day4"]["Exist"] = False
                 else:
-                    Jumat = data["Day4"]["Sub"]
-                    Jumat_ = data["Day4"]["Name"]
-                    self.Jumat = Jumat
-                    self.Day4 = Jumat_
+                    self.Datas["Days"]["Day4"]["Sub"] = data["Day4"]["Sub"]
+                    self.Datas["Days"]["Day4"]["Name"] = data["Day4"]["Name"]
 
+                # Day5
                 if data["Day5"]["Exist"] == False:
-                    self.isDay5 = False
+                    self.Datas["Days"]["Day5"]["Exist"] = False
                 else:
-                    self.isDay5 = True
-                    Sabtu = data["Day5"]["Sub"]
-                    Sabtu_ = data["Day5"]["Name"]
-                    self.Sabtu = Sabtu
-                    self.Day5 = Sabtu_
+                    self.Datas["Days"]["Day5"]["Exist"] = True
+                    self.Datas["Days"]["Day5"]["Sub"] = data["Day5"]["Sub"]
+                    self.Datas["Days"]["Day5"]["Name"] = data["Day5"]["Name"]
                     
             data_.close()
         except:
             self.isError = True
             windowError()
-        # print(type(data))
-        # print(data)
-        # print(data["app"][1]['hari'])
+
+    def windowApp(self):
+        Width = self.Size[0]
+        Height = self.Size[1]
+
+        root = tk.Tk()
+        root.title(self.Datas["Title"])
+        # root.iconbitmap("Bin\\Images\\{}".format(self.Datas["Icon"])) # Here's to change in-window Icon
+        root.resizable(False, False)
+
+        master = tk.Canvas(root, width=Width, height=Height, bg='lightgrey')
+        master.pack()
+
+        # Menu ------------------------------------------------------------
+        menubar = tk.Menu(root, bg = "grey")
+        filemenu = tk.Menu(menubar, tearoff=0, font=('open sans', 10))
+        filemenu.add_command(label="Exit", command=root.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        editmenu = tk.Menu(menubar, tearoff=0, font=('open sans', 10))
+        editmenu.add_command(label="Change Subjects", command=ChangeSubjects)
+        menubar.add_cascade(label="Edit", menu=editmenu)
+
+        helpmenu = tk.Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="Help", command=Help)
+        helpmenu.add_command(label="About...", command=About)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+
+
+        waktu = tk.LabelFrame(master, text="Time", font=('open sans', 10, "bold"))
+        waktu.grid(column=0, row=1)
+        lWaktu = tk.Listbox(waktu, width=30)
+        i = 0
+        for x in self.Datas["Time"]:
+            lWaktu.insert(i, x)
+            i += 1
+        lWaktu.itemconfigure(0, background="#ffcccb")
+        lWaktu.itemconfigure(2, background="#ffcccb")
+        lWaktu.pack()
+
+
+        # Senin ----------------------------------------------------------------------------------------
+        if self.Datas["Days"]["Day0"]["Exist"] == False:
+            pass
+        else:
+            senin = tk.LabelFrame(master, text= self.Datas["Days"]["Day0"]["Name"], font=('open sans', 10, "bold"))
+            senin.grid(column=1, row=1)
+            lSenin = tk.Listbox(senin)
+            i = 0
+            for x in self.Datas["Days"]["Day0"]["Sub"]:
+                lSenin.insert(i, x)
+                i += 1
+            lSenin.itemconfigure(0, background="#d6f0ff")
+            lSenin.itemconfigure(2, background="#d6f0ff")
+            lSenin.pack()
+
+        # Selasa ----------------------------------------------------------------------------------------
+        if self.Datas["Days"]["Day1"]["Exist"] == False:
+            pass
+        else:
+            selasa = tk.LabelFrame(master, text=self.Datas["Days"]["Day1"]["Name"], relief="groove", font=('open sans', 10, "bold"))
+            selasa.grid(column=2, row=1)
+            lSelasa = tk.Listbox(selasa, highlightcolor="white")
+            i = 0
+            for x in self.Datas["Days"]["Day1"]["Sub"]:
+                lSelasa.insert(i, x)
+                i += 1
+            lSelasa.itemconfigure(0, background="#d6f0ff")
+            lSelasa.itemconfigure(2, background="#d6f0ff")
+            lSelasa.pack()
+
+        # Rabu -----------------------------------------------------------------------------------------
+        if self.Datas["Days"]["Day2"]["Exist"] == False:
+            pass
+        else:
+            rabu = tk.LabelFrame(master, text=self.Datas["Days"]["Day2"]["Name"], relief="groove", font=('open sans', 10, "bold"))
+            rabu.grid(column=3, row=1)
+            lRabu = tk.Listbox(rabu, highlightcolor="white")
+            i = 0
+            for x in self.Datas["Days"]["Day2"]["Sub"]:
+                lRabu.insert(i, x)
+                i += 1
+            lRabu.itemconfigure(0, background="#d6f0ff")
+            lRabu.itemconfigure(2, background="#d6f0ff")
+            lRabu.pack()
+
+        # Kamis ----------------------------------------------------------------------------------------
+        if self.Datas["Days"]["Day3"]["Exist"] == False:
+            pass
+        else:
+            kamis = tk.LabelFrame(master, text=self.Datas["Days"]["Day3"]["Name"],
+                                relief="groove", font=('open sans', 10, "bold"))
+            kamis.grid(column=4, row=1)
+            lKamis = tk.Listbox(kamis, highlightcolor="white")
+            i = 0
+            for x in self.Datas["Days"]["Day3"]["Sub"]:
+                lKamis.insert(i, x)
+                i += 1
+            lKamis.itemconfigure(0, background="#d6f0ff")
+            lKamis.itemconfigure(2, background="#d6f0ff")
+            lKamis.pack()
+
+        # Jumat ----------------------------------------------------------------------------------------
+        if self.Datas["Days"]["Day4"]["Exist"] == False:
+            pass
+        else:
+            jumat = tk.LabelFrame(master, text=self.Datas["Days"]["Day4"]["Name"], relief="groove", font=('open sans', 10, "bold"))
+            jumat.grid(column=5, row=1)
+            lJumat = tk.Listbox(jumat, highlightcolor="white")
+            i = 0
+            for x in self.Datas["Days"]["Day4"]["Sub"]:
+                lJumat.insert(i, x)
+                i += 1
+            lJumat.itemconfigure(0, background="#d6f0ff")
+            lJumat.itemconfigure(2, background="#d6f0ff")
+            lJumat.pack()
+
+        # Sabtu ----------------------------------------------------------------------------------------
+        if self.Datas["Days"]["Day5"]["Exist"] == False:
+            pass
+        else:
+            sabtu = tk.LabelFrame(master, text=self.Datas["Days"]["Day5"]["Sub"], relief="groove", font=('open sans', 10, "bold"))
+            sabtu.grid(column=6, row=1)
+            lSabtu = tk.Listbox(sabtu, highlightcolor="white")
+            i = 0
+            for x in self.Datas["Days"]["Day5"]["Sub"]:
+                lSabtu.insert(i, x)
+                i += 1
+            lSabtu.itemconfigure(0, background="#d6f0ff")
+            lSabtu.itemconfigure(2, background="#d6f0ff")
+            lSabtu.pack()
+
+        bottomNavbar = tk.Canvas(root, height=30, width=Width)
+        bottomNavbar.pack(side="bottom", anchor="w")
+
         
+        # Delete Codes under this line to make compatible for various device -------------------------
+
+        # buttonNote = tk.Button(bottomNavbar, text="To-Do | Note",
+        #                        relief='groove', command=note)
+        # buttonNote.pack(side="right", padx=15, pady=7)
+
+        # buttonF = tk.Button(bottomNavbar, text="Assignment Mover",
+        #                     relief='groove', command=StartRedirect)
+        # buttonF.pack(side="right", padx=15)
+
+        # ----------------------------------------------------------------------------------------------
+        # ttk.Style().configure('TNotebook', tabposition='s')
+        root.config(menu=menubar)
+        root.mainloop()
 
 
 d = ManageData()
-
 # d.readData()
 
 
-def windowApp():
-    Width = str(700)
-    Height = str(450)
-
-    root = tk.Tk()
-    root.title(d.Title)
-    root.iconbitmap("Bin\\Images\\{}".format(d.Icon)) # Here's to change in-window Icon
-    root.resizable(False, False)
-
-    # tabControl = ttk.Notebook(root) 
-    # tab1 = ttk.Frame(tabControl)
-    # tab2 = ttk.Frame(tabControl)
-
-    # tabControl.add(tab1, text ='Schedule') 
-    # tabControl.add(tab2, text ='Tab 2') 
-    # tabControl.pack(expand = 1, fill ="both") 
-
-    master = tk.Canvas(root, width=Width, height=Height, bg='lightgrey')
-    master.pack()
-
-
-    # Menu ------------------------------------------------------------
-    menubar = tk.Menu(root, bg = "grey")
-    filemenu = tk.Menu(menubar, tearoff=0, font=('open sans', 10))
-    filemenu.add_command(label="Exit", command=root.quit)
-    menubar.add_cascade(label="File", menu=filemenu)
-
-    editmenu = tk.Menu(menubar, tearoff=0, font=('open sans', 10))
-    editmenu.add_command(label="Change Subjects", command=ChangeSubjects)
-    menubar.add_cascade(label="Edit", menu=editmenu)
-
-    helpmenu = tk.Menu(menubar, tearoff=0)
-    helpmenu.add_command(label="Help", command=Help)
-    helpmenu.add_command(label="About...", command=About)
-    menubar.add_cascade(label="Help", menu=helpmenu)
-
-    
-
-    waktu = tk.LabelFrame(master, text="Times", font=('open sans', 10, "bold"))
-    waktu.grid(column=0, row=1)
-    lWaktu = tk.Listbox(waktu, width=30)
-    i = 0
-    for x in d.Time:
-        lWaktu.insert(i, x)
-        i += 1
-    lWaktu.itemconfigure(0, background="#ffcccb")
-    lWaktu.itemconfigure(2, background="#ffcccb")
-    lWaktu.pack()
-
-    # Senin ----------------------------------------------------------------------------------------
-    if d.isDay0 == False:
-        pass
-    else:
-        senin = tk.LabelFrame(master, text=d.Day0, font=('open sans', 10, "bold"))
-        senin.grid(column=1, row=1)
-        lSenin = tk.Listbox(senin)
-        i = 0
-        for x in d.Senin:
-            lSenin.insert(i, x)
-            i += 1
-        lSenin.itemconfigure(0, background="#d6f0ff")
-        lSenin.itemconfigure(2, background="#d6f0ff")
-        lSenin.pack()
-
-    # Selasa ----------------------------------------------------------------------------------------
-    if d.isDay1 == False:
-        pass
-    else:
-        selasa = tk.LabelFrame(master, text=d.Day1, relief="groove", font=('open sans', 10, "bold"))
-        selasa.grid(column=2, row=1)
-        lSelasa = tk.Listbox(selasa, highlightcolor="white")
-        i = 0
-        for x in d.Selasa:
-            lSelasa.insert(i, x)
-            i += 1
-        lSelasa.itemconfigure(0, background="#d6f0ff")
-        lSelasa.itemconfigure(2, background="#d6f0ff")
-        lSelasa.pack()
-
-    # Rabu -----------------------------------------------------------------------------------------
-    if d.isDay2 == False:
-        pass
-    else:
-        rabu = tk.LabelFrame(master, text=d.Day2, relief="groove", font=('open sans', 10, "bold"))
-        rabu.grid(column=3, row=1)
-        lRabu = tk.Listbox(rabu, highlightcolor="white")
-        i = 0
-        for x in d.Rabu:
-            lRabu.insert(i, x)
-            i += 1
-        lRabu.itemconfigure(0, background="#d6f0ff")
-        lRabu.itemconfigure(2, background="#d6f0ff")
-        lRabu.pack()
-
-    # Kamis ----------------------------------------------------------------------------------------
-    if d.isDay3 == False:
-        pass
-    else:
-        kamis = tk.LabelFrame(master, text=d.Day3,
-                            relief="groove", font=('open sans', 10, "bold"))
-        kamis.grid(column=4, row=1)
-        lKamis = tk.Listbox(kamis, highlightcolor="white")
-        i = 0
-        for x in d.Kamis:
-            lKamis.insert(i, x)
-            i += 1
-        lKamis.itemconfigure(0, background="#d6f0ff")
-        lKamis.itemconfigure(2, background="#d6f0ff")
-        lKamis.pack()
-
-    # Jumat ----------------------------------------------------------------------------------------
-    if d.isDay4 == False:
-        pass
-    else:
-        jumat = tk.LabelFrame(master, text=d.Day4, relief="groove", font=('open sans', 10, "bold"))
-        jumat.grid(column=5, row=1)
-        lJumat = tk.Listbox(jumat, highlightcolor="white")
-        i = 0
-        for x in d.Jumat:
-            lJumat.insert(i, x)
-            i += 1
-        lJumat.itemconfigure(0, background="#d6f0ff")
-        lJumat.itemconfigure(2, background="#d6f0ff")
-        lJumat.pack()
-
-    # Sabtu ----------------------------------------------------------------------------------------
-    if d.isDay5 == False:
-        pass
-    else:
-        sabtu = tk.LabelFrame(master, text=d.Day5, relief="groove", font=('open sans', 10, "bold"))
-        sabtu.grid(column=6, row=1)
-        lSabtu = tk.Listbox(sabtu, highlightcolor="white")
-        i = 0
-        for x in d.Sabtu:
-            lSabtu.insert(i, x)
-            i += 1
-        lSabtu.itemconfigure(0, background="#d6f0ff")
-        lSabtu.itemconfigure(2, background="#d6f0ff")
-        lSabtu.pack()
-
-    bottomNavbar = tk.Canvas(root, height=20, width=Width)
-    bottomNavbar.pack(side="bottom", anchor="w")
-
-    
-    # Delete Codes under this line to make compatible for various device -------------------------
-
-    # buttonNote = tk.Button(bottomNavbar, text="To-Do | Note",
-    #                        relief='groove', command=note)
-    # buttonNote.pack(side="right", padx=15, pady=7)
-
-    # buttonF = tk.Button(bottomNavbar, text="Assignment Mover",
-    #                     relief='groove', command=StartRedirect)
-    # buttonF.pack(side="right", padx=15)
-
-    # ----------------------------------------------------------------------------------------------
-    # ttk.Style().configure('TNotebook', tabposition='s')
-    root.config(menu=menubar)
-    root.mainloop()
 
 
 
@@ -337,10 +331,13 @@ def windowApp():
 
 # Execute the App ----------------------------------------------------------------------------------
 if d.isError == False:
-    windowApp()
+    d.windowApp()
 else:
     pass
  
+# import cProfile
+# Profile
+# cProfile.run('windowApp()')
 
 
 # Convert to EXE commands --------------------------------------------------------------------------
